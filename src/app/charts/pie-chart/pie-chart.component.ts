8cae94300db26524ccb1f4244386950d6bf413b3
Joerg Weyermann <joerg.weyermann@estv.admin.ch>
2018-06-26 23:49:05 +0200

    show zh data as pie chart

--- src/app/charts/pie-chart/pie-chart.component.ts
+++ src/app/charts/pie-chart/pie-chart.component.ts
@@ -48,62 +48,83 @@ export class PieChartComponent implements OnInit {
   }
 
   loadDataJS() {
-    const width = 540;
-    const height = 540;
+    const width = 800;
+    const height = 800;
     const radius = Math.min(width, height) / 2;
 
     const svg = d3.select('#chart-area')
-        .append('svg')
-            .attr('width', width)
-            .attr('height', height)
-        .append('g')
-            .attr('transform', `translate(${width / 2}, ${height / 2})`);
+      .append('svg')
+      .attr('width', width)
+      .attr('height', height)
+      .append('g')
+      .attr('transform', `translate(${width / 2}, ${height / 2})`);
 
-    const color = d3.scaleOrdinal(['#66c2a5', '#fc8d62', '#8da0cb',
-      '#e78ac3', '#a6d854', '#ffd92f']);
+    const color = d3.scaleOrdinal(['#66c2a5', '#fc8d62', '#8da0cb', '#e78ac3', '#a6d854', '#ffd92f', '#66c2a5', '#fc8d62', '#8da0cb', '#e78ac3', '#a6d854', '#ffd92f', '#66c2a5', '#fc8d62', '#8da0cb', '#e78ac3', '#a6d854', '#ffd92f', '#66c2a5', '#fc8d62', '#8da0cb', '#e78ac3', '#a6d854', '#ffd92f', '#66c2a5', '#fc8d62', '#8da0cb', '#e78ac3', '#a6d854', '#ffd92f', '#66c2a5', '#fc8d62', '#8da0cb', '#e78ac3', '#a6d854', '#ffd92f', '#66c2a5', '#fc8d62', '#8da0cb', '#e78ac3', '#a6d854', '#ffd92f']);
 
     const pie = d3.pie()
-        .value(function(d: any) { return d.count; })
-        .sort(null);
+      .value(function (d: any) { return d.count; })
+      .sort(null);
 
     const arc = d3.arc()
-        .innerRadius(0)
-        .outerRadius(radius);
+      .innerRadius(0)
+      .outerRadius(radius);
 
     function type(d) {
-        d.apples = Number(d.apples);
-        d.oranges = Number(d.oranges);
-        return d;
+      d.apples = Number(d.apples);
+      d.oranges = Number(d.oranges);
+      return d;
     }
 
     function arcTween(a) {
-        const i = d3.interpolate(this._current, a);
-        this._current = i(1);
-        return (t) => arc(i(t));
+      const i = d3.interpolate(this._current, a);
+      this._current = i(1);
+      return (t) => arc(i(t));
     }
 
-    d3.json('assets/data/fruits.json', type).then(data => {
-        d3.selectAll('input')
-            .on('change', update);
+    // Json to load previously:
 
-        function update(val = this.value) {
-            // Join new data
-            const path = svg.selectAll('path')
-                .data(pie(data[val]));
 
-            // Update existing arcs
-            path.transition().duration(200).attrTween('d', arcTween);
+    // {
+    // 	"apples": [
+    // 		{ "region": "North", "count": "53245"},
+    // 		{ "region": "South", "count": "28479"},
+    // 		{ "region": "East", "count": "19697"},
+    // 		{ "region": "West", "count": "24037"},
+    // 		{ "region": "Central", "count": "40245"}
+    // 	],
+    // 	"oranges": [
+    // 		{ "region": "North", "count": "200"},
+    // 		{ "region": "South", "count": "200"},
+    // 		{ "region": "East", "count": "200"},
+    // 		{ "region": "West", "count": "200"},
+    // 		{ "region": "Central", "count": "200"}
+    // 	]
+    // }
 
-            // Enter new arcs
-            path.enter().append('path')
-                .attr('fill', (d, i) => color(i.toString()))
-                .attr('d', <any>arc)
-                .attr('stroke', 'white')
-                .attr('stroke-width', '6px')
-                .each(function(d) { this._current = d; });
-        }
 
-        update('apples');
+    d3.json('assets/data/pop_zh_2017_by_quarter.json', type).then(data => {
+      // d3.json('assets/data/fruits.json', type).then(data => {
+      d3.selectAll('input')
+        .on('change', update);
+
+      function update(val = this.value) {
+        // Join new data
+        const path = svg.selectAll('path')
+          .data(pie(data[val]));
+
+        // Update existing arcs
+        path.transition().duration(200).attrTween('d', arcTween);
+
+        // Enter new arcs
+        path.enter().append('path')
+          .attr('fill', (d, i) => color(i.toString()))
+          .attr('d', <any>arc)
+          .attr('stroke', 'white')
+          .attr('stroke-width', '6px')
+          .each(function (d) { this._current = d; });
+      }
+
+      update('apples');
     });
   }
 
